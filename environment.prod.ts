4de272e8e30ef3bc95ca8563cf09d43aa559e56a
IlyaBurunov <38977810+IlyaBurunov@users.noreply.github.com>
2018-05-22 13:11:10 +0300

    Add files via upload
    добавил firebase, создал макет приложения и добавил к нему стили.

---
+++ environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  config : {
+    apiKey: "AIzaSyDhbS1LNc8YG8HN9LL4Va5iIhkd9t22Sbg",
+    authDomain: "to-do-list-cb891.firebaseapp.com",
+    databaseURL: "https://to-do-list-cb891.firebaseio.com",
+    projectId: "to-do-list-cb891",
+    storageBucket: "to-do-list-cb891.appspot.com",
+    messagingSenderId: "755487945587"
+  }
+};
