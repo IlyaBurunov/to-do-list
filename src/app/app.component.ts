c021288bef81ea906656671e3a10bf88426d9ad3
IlyaBurunov <burunov1714@gmail.com>
2018-05-23 18:50:41 +0300

    add function delete and add a items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  items = ['a','b','c'];
+  newItem = '';
+  pushItem = function(){
+    if (this.newItem != ''){
+      this.items.push(this.newItem);
+      this.newItem='';
+    }
+  }
+  removeItem = function(index){
+    this.items.splice(index,1)
+  }
+
 }
